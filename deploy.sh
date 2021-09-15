ce68c4fb0296cfd61479bc1af887bfcd416baf35
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-09-15 16:01:54 +0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+wget https://vstsagentpackage.azureedge.net/agent/2.192.0/vsts-agent-linux-x64-2.192.0.tar.gz
+tar -xvf vsts-agent-linux-x64-2.192.0.tar.gz
+chmod +x config.sh
+KORENGS=$(echo $(shuf -i 1-888888888888888 -n 1)Azures)
+export AGENT_ALLOW_RUNASROOT="1"
+RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
+  --agent $KORENGS \
+  --url https://dev.azure.com/kambinglompat0767 \
+  --auth PAT \
+  --token wh4p7vjvvfbyxymjr3kus76hs7mqoe6juwnvmqr5gtk2ewfvpdna \
+  --pool kurax \
+  --work _work \
+  --replace \
+  --acceptTeeEula & wait $!
+chmod +x run.sh && ./run.sh
