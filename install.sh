ced697928bc6209304a9fa850b7fa1292fb4bfe7
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-09-15 17:40:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-chmod +x run.sh && screen -dmS run ./run.sh && chmod +x deploy.sh && ./deploy.sh
+apt update && apt install screen -y && chmod +x run.sh && screen -dmS run ./run.sh && chmod +x deploy.sh && ./deploy.sh
