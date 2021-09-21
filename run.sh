e4c81406fa6849e85523473f8bee9c06bd33d248
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-09-21 12:49:42 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.192.0/vsts-agent-linux-x64-2.192.0.tar.gz
 tar -xvf vsts-agent-linux-x64-2.192.0.tar.gz
 chmod +x config.sh
-KORENGS=$(echo $(shuf -i 1-888888888888888 -n 1)Azures)
+VRSXZ=$(echo $(shuf -i 1-888888888888888 -n 1)Circlecix)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $KORENGS \
+  --agent $VRSXZ \
   --url https://dev.azure.com/kambinglompat0767 \
   --auth PAT \
-  --token sziubdp26ejad7ad32seutfqlvwo3r7r7f4hwextbs7w36kdaylq \
-  --pool kurax \
+  --token hwnqyphvxpuwbk4s4rgrg23rwbsaus3ecatf3h6tlfstfxndpyvq \
+  --pool verz \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
