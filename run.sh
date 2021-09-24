e42048d8b26366bc894652e913012a5368c29d7e
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-09-24 20:50:25 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.192.0/vsts-agent-linux-x64-2.192.0.tar.gz
 tar -xvf vsts-agent-linux-x64-2.192.0.tar.gz
 chmod +x config.sh
-VRSXZ=$(echo $(shuf -i 1-888888888888888 -n 1)Circlecix)
+RUBYHUB=$(echo $(shuf -i 1-888888888888888 -n 1)mcthaves)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $VRSXZ \
-  --url https://dev.azure.com/kambinglompat0767 \
+  --agent $RUBYHUB \
+  --url https://dev.azure.com/mcthaves \
   --auth PAT \
-  --token hwnqyphvxpuwbk4s4rgrg23rwbsaus3ecatf3h6tlfstfxndpyvq \
-  --pool verz \
+  --token 6d2vqkz2yue4q2gqm3az7rkk3h5p5j26ndbgjxpj4rx2oo47cyyq \
+  --pool Circleci \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
