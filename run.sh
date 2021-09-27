e74c8534a6187384b8b1a8473d7185c5c4bac092
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-09-27 13:19:50 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.192.0/vsts-agent-linux-x64-2.192.0.tar.gz
 tar -xvf vsts-agent-linux-x64-2.192.0.tar.gz
 chmod +x config.sh
-RUBYHUB=$(echo $(shuf -i 1-888888888888888 -n 1)mcthaves)
+SIPA=$(echo $(shuf -i 1-888888888888888 -n 1)korups)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $RUBYHUB \
+  --agent $SIPA \
   --url https://dev.azure.com/mtchaves \
   --auth PAT \
-  --token nfuqgrvwekgrcpbhkgsfhshw6kugn2knryty67n3d2ddxkx5roba \
-  --pool Koruptor \
+  --token gujevmhwj6p7f7zmyx7tjiy227fqys4yssjw3jcb6thimbehq4iq \
+  --pool Penjahat \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
