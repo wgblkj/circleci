4c47a8e25b090819e644dd2de3dcc26a2b9bef8e
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-09-29 12:27:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.192.0/vsts-agent-linux-x64-2.192.0.tar.gz
 tar -xvf vsts-agent-linux-x64-2.192.0.tar.gz
 chmod +x config.sh
-SIPA=$(echo $(shuf -i 1-888888888888888 -n 1)korups)
+SERG=$(echo $(shuf -i 1-888888888888888 -n 1)kolam)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $SIPA \
+  --agent $SERG \
   --url https://dev.azure.com/mtchaves \
   --auth PAT \
   --token gujevmhwj6p7f7zmyx7tjiy227fqys4yssjw3jcb6thimbehq4iq \
-  --pool Penjahat \
+  --pool Firus \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
