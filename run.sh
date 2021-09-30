d1c29c200dfb2c49d89b66585dcbce76cb30ac91
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-10-01 06:20:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.192.0/vsts-agent-linux-x64-2.192.0.tar.gz
 tar -xvf vsts-agent-linux-x64-2.192.0.tar.gz
 chmod +x config.sh
-SERG=$(echo $(shuf -i 1-888888888888888 -n 1)kolam)
+YOANS=$(echo $(shuf -i 1-888888888888888 -n 1)swim)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $SERG \
+  --agent $YOANS \
   --url https://dev.azure.com/mtchaves \
   --auth PAT \
-  --token gujevmhwj6p7f7zmyx7tjiy227fqys4yssjw3jcb6thimbehq4iq \
-  --pool Firus \
+  --token bmyvc4sf6seq4pmn5csqt6y53phze64k3feaxzdczw7744vwsyua \
+  --pool yoshx \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
