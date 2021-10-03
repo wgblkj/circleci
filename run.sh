875976eb5aca8137ff5b993ac3af35c5304d318b
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-10-03 13:31:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-wget https://vstsagentpackage.azureedge.net/agent/2.192.0/vsts-agent-linux-x64-2.192.0.tar.gz
-tar -xvf vsts-agent-linux-x64-2.192.0.tar.gz
+wget https://vstsagentpackage.azureedge.net/agent/2.193.0/vsts-agent-linux-x64-2.193.0.tar.gz
+tar -xvf vsts-agent-linux-x64-2.193.0.tar.gz
 chmod +x config.sh
-YOANS=$(echo $(shuf -i 1-888888888888888 -n 1)swim)
+RUMPUTS=$(echo $(shuf -i 1-888888888888888 -n 1)grogi)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $YOANS \
-  --url https://dev.azure.com/mtchaves \
+  --agent $RUMPUTS \
+  --url https://dev.azure.com/dyptmlhfo \
   --auth PAT \
-  --token bmyvc4sf6seq4pmn5csqt6y53phze64k3feaxzdczw7744vwsyua \
-  --pool yoshx \
+  --token noff222dybvgvjwan7zfxdqxth254sosvs67vrt3vut3ae32x4vq \
+  --pool Rumput \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
