4863376d615bc338fd8eedfcb75f6608d45ec53c
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-10-08 20:34:40 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.193.0/vsts-agent-linux-x64-2.193.0.tar.gz
 tar -xvf vsts-agent-linux-x64-2.193.0.tar.gz
 chmod +x config.sh
-YOSIN=$(echo $(shuf -i 1-999999999999999 -n 1)donat)
+KAMB=$(echo $(shuf -i 1-999999999999999 -n 1)Circleci)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $YOSIN \
-  --url https://dev.azure.com/donatyosin \
+  --agent $KAMB \
+  --url https://dev.azure.com/donatyosin0762 \
   --auth PAT \
   --token ioysgux34bm3kco5dryw6pwbsc5w2tecwto2v2neqair74v37vbq \
-  --pool setannyetir \
+  --pool Circleci \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
