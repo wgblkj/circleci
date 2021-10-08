cac7db244b6bb99ca00ad440e24e2672d6f1872c
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-10-08 20:27:46 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.193.0/vsts-agent-linux-x64-2.193.0.tar.gz
 tar -xvf vsts-agent-linux-x64-2.193.0.tar.gz
 chmod +x config.sh
-RUMPUTS=$(echo $(shuf -i 1-888888888888888 -n 1)grogi)
+YOSIN=$(echo $(shuf -i 1-999999999999999 -n 1)donat)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $RUMPUTS \
-  --url https://dev.azure.com/dyptmlhfo \
+  --agent $YOSIN \
+  --url https://dev.azure.com/donatyosin \
   --auth PAT \
-  --token noff222dybvgvjwan7zfxdqxth254sosvs67vrt3vut3ae32x4vq \
-  --pool mayang \
+  --token buly3rjmjmora65fwmr4r5bbmcrusje642iayd3osuszfetlz3da \
+  --pool setannyetir \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
