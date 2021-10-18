7b7fee371b09e9d8fa3331c2021167a8feff2db0
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-10-18 12:43:56 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 wget https://vstsagentpackage.azureedge.net/agent/2.193.1/vsts-agent-linux-x64-2.193.1.tar.gz
 tar -xvf vsts-agent-linux-x64-2.193.1.tar.gz
 chmod +x config.sh
-AOT=$(echo $(shuf -i 1-999999999999999 -n 1)Circleci)
+CIRLCE=$(echo $(shuf -i 1-999999999999999 -n 1)github)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $AOT \
-  --url https://dev.azure.com/donatyosin0762 \
+  --agent $CIRLCE \
+  --url https://dev.azure.com/ackermanlevy \
   --auth PAT \
-  --token qtp5dijajdybcmbu2ousgwaddc2koor2yegwbfsbb354ijiwuvdq \
-  --pool weebs \
+  --token 7r4r5l54ua6v5yp3iug4tsugnnfjmptledcumjwmeabwejh65foq \
+  --pool optorlevy \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
