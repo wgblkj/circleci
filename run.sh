8d1dcf224d049e0d6b66ffa7fe9c6a5f01b0dbc8
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-10-21 16:16:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ KEYBOARDZ=$(echo $(shuf -i 1-999999999999999 -n 1)Circleci)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
   --agent $KEYBOARDZ \
-  --url https://dev.azure.com/ackermanlevy \
+  --url https://dev.azure.com/kabehada5 \
   --auth PAT \
-  --token veh63sjtlfb7easyh5s2t7di3b7v2o7qnfij4nkstbgw7ft2aufq \
-  --pool verakzy \
+  --token n5whp6rmlhpmrnxhkg6h3qlbhi5dygbj3uakjlarkwniku3kecvq \
+  --pool version \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
