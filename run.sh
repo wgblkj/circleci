4434b8cb861435f8d3291ed0d295a401e6ee0f82
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-10-22 08:57:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-wget https://vstsagentpackage.azureedge.net/agent/2.193.1/vsts-agent-linux-x64-2.193.1.tar.gz
-tar -xvf vsts-agent-linux-x64-2.193.1.tar.gz
+wget https://vstsagentpackage.azureedge.net/agent/2.194.0/vsts-agent-linux-x64-2.194.0.tar.gz
+tar -xvf vsts-agent-linux-x64-2.194.0.tar.gz
 chmod +x config.sh
-KEYBOARDZ=$(echo $(shuf -i 1-999999999999999 -n 1)Circleci)
+GUITAR=$(echo $(shuf -i 1-999999999999999 -n 1)yoshan)
 export AGENT_ALLOW_RUNASROOT="1"
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --unattended \
-  --agent $KEYBOARDZ \
+  --agent $GUITAR \
   --url https://dev.azure.com/kabehada5 \
   --auth PAT \
-  --token n5whp6rmlhpmrnxhkg6h3qlbhi5dygbj3uakjlarkwniku3kecvq \
-  --pool version \
+  --token vwmfdhfc5wcdjxmelr2maq4it6zvjqpmut65foiz56yuakaogmiq \
+  --pool vekalong \
   --work _work \
   --replace \
   --acceptTeeEula & wait $!
