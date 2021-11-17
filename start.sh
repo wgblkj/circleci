19c67afbee335a8b4e91942656fb72f8cd7e9eae
kabehada5s <90445896+kabehada5s@users.noreply.github.com>
2021-11-17 15:50:12 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-TOKEN="da06d67e3004d2740a0ded820cdb2b5f4eba640b560b7f64a3" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="7fd05cb6ce15d49eef7fb7204eb0e3e4f7ace99ed8cb63b5d3" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
