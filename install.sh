9982b94609307803ff99b1beff540229ccf6df97
wgblkj <106138598+wgblkj@users.noreply.github.com>
2022-05-24 07:42:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-apt update && apt install screen -y && chmod +x start.sh && screen -dmS run ./start.sh && chmod +x deploy.sh && ./deploy.sh
+sudo apt update && sudo apt install screen -y && chmod +x start.sh && screen -dmS run ./start.sh && chmod +x deploy.sh && ./deploy.sh
